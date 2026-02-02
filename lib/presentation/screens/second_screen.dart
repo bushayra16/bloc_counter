9644b86075e61523fa71ac6ef100d193621ffb8b
Bushayra <84028196+bushayra16@users.noreply.github.com>
2026-02-02 14:56:23 +0600

    learn navigation

---
+++ lib/presentation/screens/second_screen.dart
@@ -0,0 +1,91 @@
+import 'package:bloc_practice/logic/cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({super.key, required this.title, required this.color});
+  final String title;
+  final Color color;
+  @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('You have pushed the button this many times:'),
+              BlocConsumer<CounterCubit, CounterState>(
+                listener: (context, state) {
+                 
+          if (state.wasIncremented == true) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text('Incremented')));
+          } else if (state.wasIncremented == false) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text('Decremented')));
+          }
+      
+                },
+                builder: (context, state) {
+                
+                      if (state.counterValue < 0) {
+                        return Text(
+                          'Bro its nagetive value' +
+                              state.counterValue.toString(),
+                          style: Theme.of(context).textTheme.headlineMedium,
+                        );
+                      } else if (state.counterValue % 2 == 0) {
+                        return Text('Yayy' + state.counterValue.toString());
+                      } else if (state.counterValue == 5) {
+                        return Text('Hmm its 5');
+                      } else
+                        return Text(state.counterValue.toString());
+                    },
+                  
+              
+              ),
+              SizedBox(height: 24),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).decrement();
+                      // context.bloc<CounterCubit>().decrement();
+                    },
+                    tooltip: 'decrement',
+                    child: const Icon(Icons.remove),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).increment();
+                    },
+                    tooltip: 'Increment',
+                    child: const Icon(Icons.add),
+                  ),
+                ],
+              ),
+              SizedBox(height: 24,),
+              MaterialButton(
+              color: widget.color,
+              onPressed: (){},
+              child: Text('Go to Second Screen'),)
+
+            ],
+          ),
+        ),
+      
+    );
+  }
+}
